82a74aa4738f66fc096d2a5bb48c00e35a11d0aa
Błażej Jaworowski <01151621@pw.edu.pl>
2021-12-07 20:52:41 +0100

    Added migrations and changed database seeding.

--- CarRental/Data/DatabaseContext.cs
+++ CarRental/Data/DatabaseContext.cs
@@ -21,6 +21,45 @@ namespace CarRental.Data
                 .HasIndex(c => c.Horsepower);
             modelBuilder.Entity<Models.Car>()
                 .HasIndex(c => c.YearOfProduction);
+
+            modelBuilder.Entity<Models.Car>()
+                .HasData(new Models.Car[]
+                {
+                    new Models.Car
+                    {
+                        Id = 1,
+                        Brand = "Lego",
+                        Model = "Custom Model",
+                        Description = "Taki fajny kolorowy i szybki",
+                        Horsepower = 9999,
+                        YearOfProduction = 2040
+                    }
+                });
+
+            modelBuilder.Entity<Models.User>()
+                .HasData(new Models.User[]
+                {
+                    new Models.User
+                    {
+                        Id = 1,
+                        AuthID = "googleid2",
+                        DateOfBirth = System.DateTime.Today,
+                        DriversLicenseDate = System.DateTime.MinValue,
+                        Email = "user2@website.com",
+                        Location = "Warsaw",
+                        Role = Models.User.UserRole.ADMINISTRATOR
+                    },
+                    new Models.User
+                    {
+                        Id = 2,
+                        AuthID = "googleid",
+                        DateOfBirth = System.DateTime.Today,
+                        DriversLicenseDate = System.DateTime.MinValue,
+                        Email = "user@website.com",
+                        Location = "Warsaw",
+                        Role = Models.User.UserRole.CLIENT
+                    }
+                });
         }
     }
 }
