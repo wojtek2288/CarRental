be24ee61d5a90652edfd029d10930acf72988bf5
Błażej Jaworowski <01151621@pw.edu.pl>
2021-12-07 20:16:02 +0100

    Added Cars table indexes.

--- CarRental/Data/DatabaseContext.cs
+++ CarRental/Data/DatabaseContext.cs
@@ -10,5 +10,17 @@ namespace CarRental.Data
 
         public DbSet<Models.User> Users { get; set; }
         public DbSet<Models.Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Car>()
+                .HasIndex(c => c.Brand);
+            modelBuilder.Entity<Models.Car>()
+                .HasIndex(c => c.Model);
+            modelBuilder.Entity<Models.Car>()
+                .HasIndex(c => c.Horsepower);
+            modelBuilder.Entity<Models.Car>()
+                .HasIndex(c => c.YearOfProduction);
+        }
     }
 }
