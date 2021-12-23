b6e8690cf5110518d97c54794413aa1541d21237
Błażej Jaworowski <01151621@pw.edu.pl>
2021-12-23 18:08:43 +0100

    Added Quota to database.

---
+++ CarRental/Models/Quota.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CarRental.Models
+{
+    public class Quota
+    {
+        public Guid Id { get; set; }
+        public int Price { get; set; }
+        public string Currency { get; set; }
+        public DateTime ExpiredAt { get; set; }
+        public Guid CarId { get; set; }
+        public Guid UserId { get; set; }
+        public int RentDuration { get; set; }
+    }
+}
