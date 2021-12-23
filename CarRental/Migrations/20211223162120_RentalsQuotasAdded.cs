3344692abf2bd7d9522dc51de56c9d79095f37a4
Błażej Jaworowski <01151621@pw.edu.pl>
2021-12-23 18:09:26 +0100

    Changed Rental classes and added migrations.

---
+++ CarRental/Migrations/20211223162120_RentalsQuotasAdded.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRental.Migrations
+{
+    public partial class RentalsQuotasAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Cars",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateTable(
+                name: "Quotas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExpiredAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CarId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RentDuration = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rentals",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    From = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    To = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rentals", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "Brand", "Description", "Horsepower", "Model", "TimeAdded", "YearOfProduction" },
+                values: new object[] { new Guid("de8725ba-e24d-4bea-b3eb-61f459c4b0c3"), "Lego", "Taki fajny kolorowy i szybki", 9999, "Custom Model", new DateTime(2021, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2040 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "AuthID", "DateOfBirth", "DriversLicenseDate", "Email", "Location", "Role" },
+                values: new object[] { new Guid("bbc591e4-eb41-4f8d-a030-1e892393224a"), "googleid2", new DateTime(2021, 12, 23, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "user2@website.com", "Warsaw", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "AuthID", "DateOfBirth", "DriversLicenseDate", "Email", "Location", "Role" },
+                values: new object[] { new Guid("c72d2e73-93b6-4ddb-bf6e-c778dd425e6b"), "googleid", new DateTime(2021, 12, 23, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "user@website.com", "Warsaw", 0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotas");
+
+            migrationBuilder.DropTable(
+                name: "Rentals");
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: new Guid("de8725ba-e24d-4bea-b3eb-61f459c4b0c3"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("bbc591e4-eb41-4f8d-a030-1e892393224a"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("c72d2e73-93b6-4ddb-bf6e-c778dd425e6b"));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "Brand", "Description", "Horsepower", "Model", "TimeAdded", "YearOfProduction" },
+                values: new object[] { 1, "Lego", "Taki fajny kolorowy i szybki", 9999, "Custom Model", new DateTime(2021, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2040 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "AuthID", "DateOfBirth", "DriversLicenseDate", "Email", "Location", "Role" },
+                values: new object[] { 1, "googleid2", new DateTime(2021, 12, 7, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "user2@website.com", "Warsaw", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "AuthID", "DateOfBirth", "DriversLicenseDate", "Email", "Location", "Role" },
+                values: new object[] { 2, "googleid", new DateTime(2021, 12, 7, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "user@website.com", "Warsaw", 0 });
+        }
+    }
+}
