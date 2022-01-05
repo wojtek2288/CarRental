e336d8c0be23dda9ac0dde3d5e6724b983e35049
Błażej Jaworowski <01151621@pw.edu.pl>
2022-01-06 00:30:51 +0100

    Added DbUtils unit tests

---
+++ CarRentalTests/DbMock.cs
@@ -0,0 +1,100 @@
+﻿using CarRental.Data;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalTests
+{
+    internal class DbMock
+    {
+        private Mock<DatabaseContext> mockContext;
+        private Mock<DbSet<CarRental.Models.User>> mockUsers;
+        private Mock<DbSet<CarRental.Models.Car>> mockCars;
+        private Mock<DbSet<CarRental.Models.Rental>> mockRentals;
+        private Mock<DbSet<CarRental.Models.Quota>> mockQuotas;
+
+        public DbMock()
+        {
+            var userData = new List<CarRental.Models.User>() {
+                    new CarRental.Models.User
+                    {
+                        Id = Guid.Parse("bbc591e4-eb41-4f8d-a030-1e892393224a"),
+                        AuthID = "googleid2",
+                        DateOfBirth = System.DateTime.Today,
+                        DriversLicenseDate = System.DateTime.MinValue,
+                        Email = "user2@website.com",
+                        Location = "Warsaw",
+                        Role = CarRental.Models.User.UserRole.ADMINISTRATOR
+                    },
+                    new CarRental.Models.User
+                    {
+                        Id = Guid.Parse("c72d2e73-93b6-4ddb-bf6e-c778dd425e6b"),
+                        AuthID = "googleid",
+                        DateOfBirth = System.DateTime.Today,
+                        DriversLicenseDate = System.DateTime.MinValue,
+                        Email = "user@website.com",
+                        Location = "Warsaw",
+                        Role = CarRental.Models.User.UserRole.CLIENT
+                    }
+            }.AsQueryable();
+            mockUsers = new Mock<DbSet<CarRental.Models.User>>();
+            mockUsers.As<IQueryable<CarRental.Models.User>>().Setup(m => m.Provider).Returns(userData.Provider);
+            mockUsers.As<IQueryable<CarRental.Models.User>>().Setup(m => m.Expression).Returns(userData.Expression);
+            mockUsers.As<IQueryable<CarRental.Models.User>>().Setup(m => m.ElementType).Returns(userData.ElementType);
+            mockUsers.As<IQueryable<CarRental.Models.User>>().Setup(m => m.GetEnumerator()).Returns(userData.GetEnumerator());
+
+            var carData = new List<CarRental.Models.Car>() {
+                    new CarRental.Models.Car
+                    {
+                        Id = Guid.Parse("de8725ba-e24d-4bea-b3eb-61f459c4b0c3"),
+                        Brand = "Lego",
+                        Model = "Custom Model",
+                        Description = "Taki fajny kolorowy i szybki",
+                        Horsepower = 9999,
+                        YearOfProduction = 2040,
+                        TimeAdded = new System.DateTime(2021, 12, 1)
+                    }
+            }.AsQueryable();
+            mockCars = new Mock<DbSet<CarRental.Models.Car>>();
+            mockCars.As<IQueryable<CarRental.Models.Car>>().Setup(m => m.Provider).Returns(carData.Provider);
+            mockCars.As<IQueryable<CarRental.Models.Car>>().Setup(m => m.Expression).Returns(carData.Expression);
+            mockCars.As<IQueryable<CarRental.Models.Car>>().Setup(m => m.ElementType).Returns(carData.ElementType);
+            mockCars.As<IQueryable<CarRental.Models.Car>>().Setup(m => m.GetEnumerator()).Returns(carData.GetEnumerator());
+
+            var rentalData = new List<CarRental.Models.Rental>()
+            {
+            }.AsQueryable();
+            mockRentals = new Mock<DbSet<CarRental.Models.Rental>>();
+            mockRentals.As<IQueryable<CarRental.Models.Rental>>().Setup(m => m.Provider).Returns(rentalData.Provider);
+            mockRentals.As<IQueryable<CarRental.Models.Rental>>().Setup(m => m.Expression).Returns(rentalData.Expression);
+            mockRentals.As<IQueryable<CarRental.Models.Rental>>().Setup(m => m.ElementType).Returns(rentalData.ElementType);
+            mockRentals.As<IQueryable<CarRental.Models.Rental>>().Setup(m => m.GetEnumerator()).Returns(rentalData.GetEnumerator());
+
+            var quotaData = new List<CarRental.Models.Quota>()
+            {
+            }.AsQueryable();
+            mockQuotas = new Mock<DbSet<CarRental.Models.Quota>>();
+            mockQuotas.As<IQueryable<CarRental.Models.Quota>>().Setup(m => m.Provider).Returns(quotaData.Provider);
+            mockQuotas.As<IQueryable<CarRental.Models.Quota>>().Setup(m => m.Expression).Returns(quotaData.Expression);
+            mockQuotas.As<IQueryable<CarRental.Models.Quota>>().Setup(m => m.ElementType).Returns(quotaData.ElementType);
+            mockQuotas.As<IQueryable<CarRental.Models.Quota>>().Setup(m => m.GetEnumerator()).Returns(quotaData.GetEnumerator());
+
+
+            mockContext = new Mock<DatabaseContext>(new object[] { new DbContextOptions<DatabaseContext>() });
+            mockContext.Setup(m => m.Users).Returns(mockUsers.Object);
+            mockContext.Setup(m => m.Cars).Returns(mockCars.Object);
+            mockContext.Setup(m => m.Rentals).Returns(mockRentals.Object);
+            mockContext.Setup(m => m.Quotas).Returns(mockQuotas.Object);
+        }
+
+        public DatabaseContext Context { get { return mockContext.Object; } }
+        public Mock<DbSet<CarRental.Models.User>> Users { get { return mockUsers; } }
+        public Mock<DbSet<CarRental.Models.Car>> Cars { get { return mockCars; } }
+        public Mock<DbSet<CarRental.Models.Rental>> Rentals { get { return mockRentals; } }
+        public Mock<DbSet<CarRental.Models.Quota>> Quotas { get { return mockQuotas; } }
+    }
+}
---
+++ CarRentalTests/DbUtilsTests.cs
@@ -0,0 +1,112 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using CarRental.Data;
+
+namespace CarRentalTests
+{
+    [TestFixture]
+    internal class DbUtilsTests
+    {
+        DbMock dbMock;
+        DbUtils dbUtils;
+
+        [SetUp]
+        public void Setup()
+        {
+            dbMock = new();
+            dbUtils = new(dbMock.Context);
+        }
+
+        [Test]
+        public void AddUserTest()
+        {
+            var user = new CarRental.POCO.User
+            {
+                AuthID = "randomgoogleid",
+                DateOfBirth = new DateTime(2000, 2, 3),
+                DriversLicenseDate = new DateTime(2018, 4, 5),
+                Email = "randomemail@randomsite.com",
+                Location = "Warsaw",
+                Role = CarRental.POCO.User.UserRole.CLIENT
+            };
+            dbUtils.AddUser(user);
+
+            dbMock.Users.Verify(m => m.Add(It.Is<CarRental.Models.User>(arg => arg.AuthID == user.AuthID &&
+                                                                            arg.DateOfBirth == user.DateOfBirth &&
+                                                                            arg.DriversLicenseDate == user.DriversLicenseDate &&
+                                                                            arg.Email == user.Email &&
+                                                                            arg.Location == user.Location &&
+                                                                            arg.Role == CarRental.Models.User.UserRole.CLIENT)), Times.Once);
+        }
+
+        [Test]
+        public void AddCarTest()
+        {
+            var car = new CarRental.POCO.Car
+            {
+                Brand = "Volvo",
+                Description = "Gitowy",
+                Horsepower = 100,
+                Model = "Jakis",
+                YearOfProduction = 2000
+            };
+            dbUtils.AddCar(car);
+
+            dbMock.Cars.Verify(m => m.Add(It.Is<CarRental.Models.Car>(arg => arg.Brand == car.Brand &&
+                                                                          arg.Description == car.Description &&
+                                                                          arg.Horsepower == car.Horsepower &&
+                                                                          arg.Model == car.Model &&
+                                                                          arg.YearOfProduction == car.YearOfProduction)), Times.Once);
+        }
+
+        [Test]
+        public void AddRentalTest()
+        {
+            var rental = new CarRental.POCO.Rental
+            {
+                CarId = new Guid(),
+                Currency = "USD",
+                Price = 100,
+                From = DateTime.Today.AddDays(2),
+                To = DateTime.Today.AddDays(10),
+                UserId = new Guid()
+            };
+            dbUtils.AddRental(rental);
+
+            dbMock.Rentals.Verify(m => m.Add(It.Is<CarRental.Models.Rental>(arg => arg.CarId == rental.CarId &&
+                                                                                arg.Currency == rental.Currency &&
+                                                                                arg.Price == rental.Price &&
+                                                                                arg.From == rental.From &&
+                                                                                arg.To == rental.To &&
+                                                                                arg.UserId == rental.UserId)));
+        }
+
+        [Test]
+        public void AddQuotaTest()
+        {
+            var quota = new CarRental.POCO.Quota
+            {
+                CarId = new Guid(),
+                Currency = "USD",
+                ExpiredAt = DateTime.Now,
+                Price = 100,
+                RentDuration = 2,
+                UserId = new Guid()
+            };
+            dbUtils.AddQuota(quota);
+
+            dbMock.Quotas.Verify(m => m.Add(It.Is<CarRental.Models.Quota>(arg => arg.CarId == quota.CarId &&
+                                                                              arg.Currency == quota.Currency &&
+                                                                              arg.ExpiredAt == quota.ExpiredAt &&
+                                                                              arg.Price == quota.Price &&
+                                                                              arg.RentDuration == quota.RentDuration &&
+                                                                              arg.UserId == quota.UserId)));
+        }
+    }
+}
