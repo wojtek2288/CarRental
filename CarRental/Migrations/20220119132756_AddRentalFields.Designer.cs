615027a3df01f135bbb9c933af8aabeb85b3b012
Błażej Jaworowski <01151621@pw.edu.pl>
2022-01-19 14:30:45 +0100

    Added a migration

--- CarRental/Migrations/20220119132756_AddRentalFields.Designer.cs
+++ CarRental/Migrations/20220119132756_AddRentalFields.Designer.cs
@@ -4,14 +4,16 @@ using CarRental.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CarRental.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220119132756_AddRentalFields")]
+    partial class AddRentalFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
